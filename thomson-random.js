eadcef189618ef15606b0eb21f2be529a82bf455
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2013-02-12 03:30:37 -0500

    annealing approach

--- thomson-random.js
+++ thomson-random.js
@@ -53,15 +53,14 @@ $(function() {
     var energyKernel = tmcl.compile(energyKernelSource, 'clEnergyKernel');
     var resultHandle = tmcl.toGPU(result);
 
-    // generate a new, random set of points
-    generate(points, n);
-
     // try a different number of energy computations
-    var runs = 5;
-    var dt = 1;
+    var runs = 10;
     var min = Number.MAX_VALUE;
     var energies = [];
     for (var i = 0; i < runs; i++) {
+        // generate a new, random set of points
+        generate(points, n);
+
         // send data to gpu
         var pointsHandle = tmcl.toGPU(points);
 
@@ -73,28 +72,14 @@ $(function() {
             global: global
         }, pointsHandle, resultHandle, new Int32(n));
 
-        // get energies from GPU, check if we found a better configuration
+        // get energies from GPU, and check if we found a better configuration
         tmcl.fromGPU(resultHandle, result);
         var e = energy(result, n);
         if (e < min)
             min = e;
 
-        // remember all computed energies
+        // remember all energies
         energies.push(e);
-
-        // compute new locations for points
-        for (var j = 0; j < n; j++) {
-            // multiply each point by the force acting upon it
-            points[3 * j] = Math.abs(points[3 * j] * result[j] * dt);
-            points[3 * j + 1] = Math.abs(points[3 * j + 1] * result[j] * dt);
-            points[3 * j + 2] = Math.abs(points[3 * j + 2] * result[j] * dt);
-
-            // re-normalize coordinates
-            var length = Math.sqrt(Math.pow(points[3 * j], 2) + Math.pow(points[3 * j + 1], 2) + Math.pow(points[3 * j + 2], 2));
-            points[3 * j] = points[3 * j] / length;
-            points[3 * j + 1] = points[3 * j + 1] / length;
-            points[3 * j + 2] = points[3 * j + 2] / length;
-        }
     }
 
     console.log('Energies', energies);
